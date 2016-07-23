0c70b1e62345a8b9032704e66f9877829c8d9003
Rodrigo Rocha <rodrigorgs@gmail.com>
2016-07-23 11:36:57 -0300

    Create object that moves with time.

---
+++ Assets/TimeBehavior.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeBehavior : MonoBehaviour {
+
+	private Vector3 initialPos;
+	public Vector3 finalPos;
+	public float initialTime;
+	public float finalTime;
+	public float xxxCurrentTime;
+
+	// Use this for initialization
+	void Start () {
+		initialPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (xxxCurrentTime <= initialTime) {
+			transform.position = initialPos;
+		} else if (xxxCurrentTime >= finalTime) {
+			transform.position = finalPos;
+		} else {
+			float x, y, z;
+			float alpha = (xxxCurrentTime - initialTime) / (finalTime - initialTime);
+			Vector3 delta = finalPos - initialPos;
+			x = initialPos.x + alpha * delta.x;
+			y = initialPos.y + alpha * delta.y;
+			z = initialPos.z + alpha * delta.z;
+			transform.position = new Vector3 (x, y, z);
+		}
+	}
+}
---
+++ Assets/TimeManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeManager : MonoBehaviour {
+
+	public float tempo = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
